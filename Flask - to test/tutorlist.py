6a6b3aebe36e82e222f328ace46b111ad45de0e8
amystell <60944724+amystell@users.noreply.github.com>
2020-03-06 13:30:31 +0000

    tutors from db

--- Flask - to test/tutorlist.py
+++ Flask - to test/tutorlist.py
@@ -4,7 +4,7 @@ def tutor_list():
     cur = mysql.connection.cursor()
     cur.execute(''' SELECT title, fName, lName FROM Tutor''')
     result = cur.fetchall()
-    s = ""
+    s = "<option class=\"options\" value=\"\" disabled selected hidden>Please select a tutor</option>"
     for x in result:
         s += "<option class=\"options\" value="+str(x['fName'])+">"+str(x['title'])+" "+str(x['fName'])+" "+str(x['lName'])+"</option>"
     return s
