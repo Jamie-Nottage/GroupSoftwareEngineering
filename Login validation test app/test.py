66577f550768558af6a2ba24d4afc403cf459bb0
Jamie-Nottage <56916511+Jamie-Nottage@users.noreply.github.com>
2020-03-10 21:24:25 +0000

    Placed all code and templates into login test app
    Placed all code together into the login test app to test the connection between all the pages and the functionality.

--- Login validation test app/test.py
+++ Login validation test app/test.py
@@ -28,36 +28,35 @@ def home_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	error = None
 	if request.method == 'POST':
 		if 'username' in session:
 			return redirect(url_for('index'))
 		else:
-			if validateLogin(request.form["username"], request.form["password"]) == 0:
+			valid = validateLogin(request.form["username"], request.form["password"])
+			if valid == 0:
 				return redirect(url_for('main_app'))
+			elif valid == 1:
+				return redirect(url_for('game_keeper'))
 			else:
-				return 'Not valid login'
-
-@app.route('/login-page')
-def login_page():
-	return render_template('login-page.html')
+				error = True
+	return render_template('login-page.html', loginError=error)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+	error = None
 	if request.method == 'POST':
 		if verifySignup(request.form["firstname"], request.form["surname"], request.form["email"], request.form["username"], request.form["password"], request.form["tutor"]) == 0:
 			return redirect(url_for('main_app'))
 		else:
-			return 'Signup failed'
-
-@app.route('/signup-page')
-def signup_page():
-	return render_template('signup-page.html')
+			error = True
+	return render_template('signup-page.html', signupError=error)
 
 @app.route('/getTutors', methods=['POST','GET'])
 def get_tutors():
 	if request.method == 'POST':
 		return tutor_list()
-	return redirect(url_for('signup-page'))
+	return redirect(url_for('signup'))
 	
 @app.route('/terms')
 def terms_page():
@@ -66,7 +65,6 @@ def terms_page():
 @app.route('/privacy')
 def privacy_page():
 	return render_template('privacy-page.html')
-
 	
 # adding tutors into drop down list in signup page
 def tutor_list():
@@ -80,16 +78,19 @@ def tutor_list():
 
 def validateLogin(username, password):
 	cur = mysql.connection.cursor()
-	cur.execute('''SELECT * FROM Users WHERE username=\'%s\';''' % username)
+	cur.execute('''SELECT username, password FROM Users WHERE username=\'%s\' LIMIT 1;''' % username)
 	user = cur.fetchall()
-	if not user:
-		return 1
-	else:
+	cur.execute('''SELECT username, password FROM Gamekeeper WHERE username=\'%s\' LIMIT 1;''' % username)
+	tutor = cur.fetchall()
+	if user:
 		hashedPassword = user[0]['password']
 		if sha256_crypt.verify(password,hashedPassword):
 			return 0
-	return 1
-
+	elif tutor:
+		hashedPassword = tutor[0]['password']
+		if sha256_crypt.verify(password,hashedPassword):
+			return 1
+	return -1
 
 def verifySignup(name, surname, email, username, password, tutor):
 	cur = mysql.connection.cursor()
@@ -109,7 +110,103 @@ def verifySignup(name, surname, email, username, password, tutor):
 		mysql.connection.commit()
 		return 0
 		
-#BEN'S CODE#		
+#BEN RAF CODE#
+@app.route('/game-keeper')
+def game_keeper():
+    return render_template('game-keeper-page.html', users=users_online(), teams=game_leader_board(), places=places_visited())
+
+@app.route('/add-tutor', methods=['POST', 'GET'])
+def add_tutor():
+    if request.method == 'POST':
+        title = request.form["title"]
+        name = request.form["firstname"]
+        surname = request.form["surname"]
+        add_tutor_db(title, name, surname)
+    return render_template('add-tutor.html')
+
+@app.route('/add-game-keeper', methods=['POST', 'GET'])
+def add_game_keeper():
+    if request.method == 'POST':
+        name = request.form["firstname"]
+        surname = request.form["surname"]
+        email = request.form["email"]
+        username = request.form["username"]
+        password = request.form["password"]
+        add_game_keeper_db(name, surname, email, username, password)
+    return render_template('add-game-keeper.html')
+
+@app.route('/get-leaderboard', methods=['POST', 'GET'])
+def get_leaderboard_gk():
+    if request.method == 'GET':
+        teams = game_leader_board()
+    return render_template('add-game-keeper.html', teams=teams)
+
+@app.route('/users-online', methods=['POST', 'GET'])
+def get_users():
+    if request.method == 'GET':
+        users = users_online()
+    return render_template('add-game-keeper.html', users=users)
+
+@app.route('/places-visited', methods=['POST', 'GET'])
+def get_places():
+    if request.method == 'GET':
+        places = places_visited()
+    return render_template('add-game-keeper.html', places=places)
+
+def add_tutor_db(title, firstname, lastname):
+    cur = mysql.connection.cursor()
+    cur.execute(''' INSERT INTO Tutor VALUES (NULL, \'%s\', \'%s\', \'%s\')''' % (title, firstname, lastname))
+    mysql.connection.commit()
+    cur.execute(''' SELECT COUNT(*) AS count from Tutor; ''')
+    tutor_count = cur.fetchall()
+    cur.execute(''' SELECT COUNT(*) AS count from Paths; ''')
+    path_count = cur.fetchall()
+    if path_count[0]['count'] < tutor_count[0]['count']:
+        cur.execute(''' INSERT INTO Paths VALUES (NULL, 6)''')
+        mysql.connection.commit()
+    cur.execute(
+        '''SELECT COUNT(*) AS count FROM Route WHERE pathId=(SELECT tutorId FROM Tutor WHERE title=\'%s\' AND fName=\'%s\' AND lName=\'%s\')''' % (
+        title, firstname, lastname))
+    route_count = cur.fetchall()
+    if route_count[0]['count'] == 0:
+        # must enter a new route before can enter a new tutor.
+        # response must be done
+        return 1
+    teamname = "Team " + firstname + " " + lastname
+    cur.execute(
+        ''' INSERT INTO Team VALUES (NULL, \'%s\', (SELECT tutorId FROM Tutor WHERE title=\'%s\' AND fName=\'%s\' AND lName=\'%s\'), (SELECT tutorId FROM Tutor WHERE title=\'%s\' AND fName=\'%s\' AND lName=\'%s\') )''' % (
+        teamname, title, firstname, lastname, title, firstname, lastname))
+    mysql.connection.commit()
+    return 0
+
+def add_game_keeper_db(name, surname, email, username, password):
+    cur = mysql.connection.cursor()
+    hashedPassword = sha256_crypt.hash(password)
+    cur.execute(
+        ''' INSERT INTO Gamekeeper (fName, lName, emailAddress, username, password) VALUES (\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')''' % (
+        name, surname, email, username, hashedPassword))
+    mysql.connection.commit()
+    return 0
+
+def game_leader_board():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM leaderboard''')
+    result = cur.fetchall()
+    return result
+
+def places_visited():
+    cur = mysql.connection.cursor()
+    cur.execute(''' SELECT COUNT(*) AS placesvisited FROM VisitBuilding''')
+    result = cur.fetchall()
+    return result
+
+def users_online():
+    cur = mysql.connection.cursor()
+    cur.execute(''' SELECT COUNT(*) AS usersonline FROM Users''')
+    result = cur.fetchall()
+    return result
+
+#BEN RANG CODE#		
 @app.route('/getSign', methods=['POST','GET'])
 def get_sign():
     if request.method == 'POST':
