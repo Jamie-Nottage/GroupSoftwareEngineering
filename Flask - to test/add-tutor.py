e1bc4d7b3bb4a05abd54d8e817034afa9ab05020
Jamie-Nottage <56916511+Jamie-Nottage@users.noreply.github.com>
2020-03-11 18:17:48 +0000

    Update add-tutor.py

--- Flask - to test/add-tutor.py
+++ Flask - to test/add-tutor.py
@@ -8,11 +8,6 @@ def add_tutor_db(title, firstname, lastname):
     cur.execute(''' SELECT COUNT(*) AS count from Paths; ''')
     path_count = cur.fetchall()
     if path_count[0]['count'] < tutor_count[0]['count']:
-        cur.execute(''' INSERT INTO Paths VALUES (NULL, 6)''')
-        mysql.connection.commit()
-    cur.execute('''SELECT COUNT(*) AS count FROM Route WHERE pathId=(SELECT tutorId FROM Tutor WHERE title=%s AND fName=%s AND lName=%s)''' %(title,firstname,lastname))
-    route_count = cur.fetchall()
-    if route_count[0]['count'] == 0:
         # must enter a new route before can enter a new tutor.
         # response must be done in JS
         return 1
