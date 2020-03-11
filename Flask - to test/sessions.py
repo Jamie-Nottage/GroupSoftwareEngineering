1bef2908c105d7ecf2359ea3e22b7f6804634c5a
amystell <60944724+amystell@users.noreply.github.com>
2020-03-11 22:21:37 +0000

    Update sessions.py

--- Flask - to test/sessions.py
+++ Flask - to test/sessions.py
@@ -10,3 +10,16 @@ def get_teamid(username):
     result = cur.fetchall()
     teamid = result[0]['teamId']
     return teamid
+
+def get_userid(username):
+    """
+    Gets the userId based on the username in the session
+    :parameter username: The username is a session variable
+    :return: Returns the teamid given the username
+    :rtype: int.
+    """
+    cur = mysql.connection.cursor()
+    cur.execute(''' SELECT userId FROM Users WHERE username=%s ''' %username)
+    result = cur.fetchall()
+    teamid = result[0]['userId']
+    return teamid
